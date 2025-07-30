39b32573a3d63eff30feb267241120446b8ed095
gzamfir-ca <gzamfir_ca@icloud.com>
2025-07-30 14:08:59 -0400

    migrate to fish

---
+++ arm64/config.fish
@@ -0,0 +1,94 @@
+if status is-interactive
+
+    # Configure brew environment
+    /opt/homebrew/bin/brew shellenv | source
+    set -gx HOMEBREW_NO_INSTALL_FROM_API 1
+
+    # Configure psql@16 binaries
+    fish_add_path --path /opt/homebrew/opt/postgresql@16/bin
+
+    # Configure node environment
+    nodenv init - | source
+
+    # Configure ruby environment
+    rbenv init - | source
+
+    # Configure lisp environment
+    fish_add_path --path /Applications/Racket\ v8.17/bin
+
+    # Configure fish environment
+    set -gx CLICOLOR 1
+    set -gx EDITOR vim
+    set -gx LSCOLORS ExFxBxDxCxegedabagacad
+    set -gx PAGER less
+
+    # Configure java environment
+    set -gx JAVA_HOME (/usr/libexec/java_home --version 21)
+    set -gx M2_HOME (readlink -f /opt/homebrew/opt/maven/libexec)
+    set -gx GRADLE_HOME (readlink -f /opt/homebrew/opt/gradle/libexec)
+
+    # Add brew cmd abbreviations
+    abbr --add brewin 'brew install --verbose'
+    abbr --add brewls 'brew list --versions (brew list --installed-on-request)'
+    abbr --add brewrm 'brew uninstall --verbose'
+    abbr --add brewup 'brew update --verbose && brew upgrade --verbose && brew cleanup --verbose'
+
+    # Add node cmd abbreviations
+    abbr --add nodein 'npm install --global'
+    abbr --add nodels 'npm ls --global --depth 1'
+    abbr --add noderm 'npm uninstall --global'
+    abbr --add nodeup 'npm install --global npm && npm update --global'
+
+    # Add ruby cmd abbreviations
+    abbr --add rubyin 'gem install'
+    abbr --add rubyls 'gem list --local --no-details | grep -v "default:"'
+    abbr --add rubyrm 'gem uninstall'
+    abbr --add rubyup 'gem update --system && gem update && gem cleanup'
+
+    # Add git args abbreviations
+    abbr --command git ad 'add -A && git commit --amend --no-edit'
+    abbr --command git am 'commit --amend -m'
+    abbr --command git ao 'remote add origin'
+    abbr --command git bl 'branch --all'
+    abbr --command git bn 'checkout -b'
+    abbr --command git br 'branch -M'
+    abbr --command git cm 'add -A && git commit -m'
+    abbr --command git co checkout
+    abbr --command git dr 'add -A --dry-run'
+    abbr --command git fe 'fetch --all && git rebase origin/$(git rev-parse --abbrev-ref HEAD)'
+    abbr --command git lg 'log --oneline --graph --decorate --stat'
+    abbr --command git ne 'init -b main && git add -A && git commit -m'
+    abbr --command git pf 'push -f origin $(git rev-parse --abbrev-ref HEAD)'
+    abbr --command git pu 'push -u origin $(git rev-parse --abbrev-ref HEAD)'
+    abbr --command git re 'remote --verbose'
+    abbr --command git rp 'remote prune origin'
+    abbr --command git st 'status --verbose'
+    abbr --command git tg 'tag -a -m'
+
+    # Make system commands safer
+    function cp --wraps cp --description 'alias cp=cp -nv'
+        command cp -nv $argv
+    end
+
+    function mv --wraps mv --description 'alias mv=mv -nv'
+        command mv -nv $argv
+    end
+
+    function rm --wraps rm --description 'alias rm=rm -iv'
+        command rm -iv $argv
+    end
+
+    # Add easier system commands
+    function gt --description 'alias gt=cd (git rev-parse --show-toplevel)'
+        cd (git rev-parse --show-toplevel)
+    end
+
+    function mc --wraps mkdir --description 'alias mc=mkdir -pv && cd'
+        command mkdir -pv $argv && cd $argv
+    end
+
+    function md --wraps mkdir --description 'alias md=mkdir -pv'
+        command mkdir -pv $argv
+    end
+
+end
