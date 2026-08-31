6098fc137e45884b2402792f47d2aeb9a6e69d11
gzamfir-ca <gzamfir_ca@icloud.com>
2026-08-31 13:20:15 -0400

    +init.el config

--- arm64/init.el
+++ arm64/init.el
@@ -84,15 +84,3 @@
 (load-theme 'catppuccin :no-confirm)
 (setq catppuccin-flavor 'macchiato)
 (catppuccin-reload)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(catppuccin-theme undohist)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
