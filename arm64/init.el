4e0420415731250d14716adb8f0adf5ca23911ee
gzamfir-ca <gzamfir_ca@icloud.com>
2026-08-31 17:27:05 -0400

    +common lisp

--- arm64/init.el
+++ arm64/init.el
@@ -92,3 +92,16 @@
 (setq inferior-lisp-program "/opt/homebrew/bin/sbcl")
 (setq split-width-threshold nil)
 (setq split-height-threshold 0)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(catppuccin-theme undohist)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
